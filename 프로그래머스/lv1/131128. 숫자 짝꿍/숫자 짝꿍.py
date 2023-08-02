c45475cf1f89e120fd2c72bd751e400ba0570fe0
Jinskim <50815599+MeluvRose@users.noreply.github.com>
2023-08-02 18:58:07 +0900

    [level 1] Title: 숫자 짝꿍, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv1/131128. 숫자 짝꿍/숫자 짝꿍.py
+++ 프로그래머스/lv1/131128. 숫자 짝꿍/숫자 짝꿍.py
@@ -17,3 +17,18 @@ def solution(X, Y):
     if output[0] == '0': return "0";
     answer = ''.join(output);
     return answer
+
+"""
+def solution(X, Y):
+    answer = ''
+
+    for i in range(9,-1,-1) :
+        answer += (str(i) * min(X.count(str(i)), Y.count(str(i))))
+
+    if answer == '' :
+        return '-1'
+    elif len(answer) == answer.count('0'):
+        return '0'
+    else :
+        return answer
+"""
