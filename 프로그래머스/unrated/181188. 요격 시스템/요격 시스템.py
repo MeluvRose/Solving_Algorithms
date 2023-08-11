778161880b82b53845d9aabdae3eb589c028f1f9
Jinskim <50815599+MeluvRose@users.noreply.github.com>
2023-08-11 23:17:22 +0900

    [unrated] Title: 요격 시스템, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181188. 요격 시스템/요격 시스템.py
+++ 프로그래머스/unrated/181188. 요격 시스템/요격 시스템.py
@@ -1,10 +1,15 @@
 def solution(targets):
     answer = 0
-    # l = 0
-    # r = 0
-    x = 0;
+    x = 0
     
+    # targets.sort() (X)
     targets.sort(key=lambda x: x[1]);
+    """
+    첫 미사일을 기준으로 새로운 미사일이 이전
+    미사일의 좌표 범위를 벗어나는 지를 확인하게
+    될 것이기에 target([s,e])의 e 좌표를 기준
+    으로 정렬했어야 하는 이유이다.
+    """
     # print(targets);
     for t in targets:
         l = t[0]
