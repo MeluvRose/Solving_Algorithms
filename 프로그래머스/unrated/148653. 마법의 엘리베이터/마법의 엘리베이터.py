27b58f01f869488abe7a234dedc15762c2c11220
Jinskim <50815599+MeluvRose@users.noreply.github.com>
2023-08-11 14:59:46 +0900

    [unrated] Title: 마법의 엘리베이터, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/148653. 마법의 엘리베이터/마법의 엘리베이터.py
+++ 프로그래머스/unrated/148653. 마법의 엘리베이터/마법의 엘리베이터.py
@@ -2,14 +2,6 @@ def solution(storey):
     answer = 0
     button = 1
     
-    # while ((storey / button) >= 10):
-    #     button *= 10;
-    # while (storey != 0):
-    #     storey = abs(storey) - button;
-    #     if abs(storey) <= (button / 2): 
-    #         button /= 10;
-    #     print(storey, button);
-    #     answer += 1;
     
     while (storey > 0):
         button = storey % 10
@@ -22,20 +14,40 @@ def solution(storey):
         answer += button;
     return answer
 
-"""
-def solution(storey):
-    answer = 0
-    while storey != 0:
-        n = storey % 10 
 
-        if n >= 6:
-            storey += 10 - n;
-            answer += 10 - n;
-        elif n == 5 and (storey // 10) % 10 >= 5:
-            storey += 10 - n;
-            answer += 10 - n;
-        else:
-            answer += n;
-        storey = storey // 10;
-    return answer
+""" 
+# 내가 생각했던 가장 단순한 알고리즘
+while ((storey / button) >= 10):
+    button *= 10;
+while (storey != 0):
+    storey = abs(storey) - button;
+    if abs(storey) <= (button / 2): 
+        button /= 10;
+    print(storey, button);
+    answer += 1;
+"""
+    
+"""
+# 정답이지만 제출 코드보다 약간 긴 코드
+while storey != 0:
+    n = storey % 10 
+
+    if n >= 6:
+        storey += 10 - n
+        answer += 10 - n
+    elif n == 5 and (storey // 10) % 10 >= 5:
+        storey += 10 - n
+        answer += 10 - n
+    else:
+        answer += n
+    storey = storey // 10
+"""
+
+"""
+# 재귀의 활용(?!)
+def solution(storey):
+    if storey < 10 :
+        return min(storey, 11 - storey)
+    left = storey % 10
+    return min(left + solution(storey // 10), 10 - left + solution(storey // 10 + 1))
 """
