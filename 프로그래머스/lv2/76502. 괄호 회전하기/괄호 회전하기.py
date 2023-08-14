ab51618f0e021f6f22d1f534bfc9e51a5302e0d8
Jinskim <50815599+MeluvRose@users.noreply.github.com>
2023-08-14 17:50:44 +0900

    [level 2] Title: 괄호 회전하기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/76502. 괄호 회전하기/괄호 회전하기.py
+++ 프로그래머스/lv2/76502. 괄호 회전하기/괄호 회전하기.py
@@ -4,18 +4,32 @@ def solution(s):
     
     for i in range(lenS):
         rotate = []
+        cnt = 0
+        for j in range(i, lenS + i):
+            if j > (lenS - 1): j -= lenS;
+            if (s[j] == ')' or s[j] == ']' or s[j] == '}'):
+                if (len(rotate) == 0): break
+                else:
+                    if ((rotate[-1] == '{' and s[j] == '}')
+                        or (rotate[-1] == '[' and s[j] == ']')
+                        or (rotate[-1] == '(' and s[j] == ')')):
+                            rotate.pop();
+                    else: break;
+            else: rotate.append(s[j]);
+            cnt += 1;
+        if (cnt == lenS and len(rotate) == 0): answer += 1;
+    return answer
+"""
+for i in range(lenS):
+        rotate = []
         for j in range(i, lenS + i):
             if j > (lenS - 1): j -= lenS;
             rotate.append(s[j]);
-        # print(rotate, end = ' ')
-        # for cnt in range(len(rotate)):
-            # part = rotate.pop()
             if (len(rotate) > 1):
                 if ((rotate[-1] == '}' and rotate[-2] == '{')
                     or (rotate[-1] == ']' and rotate[-2] == '[')
                     or (rotate[-1] == ')' and rotate[-2] == '(')):
                         rotate.pop();
                         rotate.pop();
-        # print(rotate)
         if len(rotate) == 0: answer += 1;
-    return answer
+"""
