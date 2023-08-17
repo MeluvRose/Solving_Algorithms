336c5102e4ef4500c22ac6bbed2c3ad8b34a8645
Jinskim <50815599+MeluvRose@users.noreply.github.com>
2023-08-17 17:22:50 +0900

    [level 2] Title: 기능개발, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42586. 기능개발/기능개발.py
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.py
@@ -6,12 +6,20 @@ def solution(progresses, speeds):
         deploy = 0;
         for i in range(len(features)):
             features[i] += speeds[i];
-        # if features[0] >= 100:
-        #     features.pop(0);
-        #     speeds.pop(0);
         while features and features[0] >= 100:
             deploy += 1;
             features.pop(0);
             speeds.pop(0);
         if deploy > 0: answer.append(deploy);
     return answer
+
+"""
+def solution(progresses, speeds):
+    Q=[]
+    for p, s in zip(progresses, speeds):
+        if len(Q)==0 or Q[-1][0]<-((p-100)//s):
+            Q.append([-((p-100)//s),1])
+        else:
+            Q[-1][1]+=1
+    return [q[1] for q in Q]
+"""
