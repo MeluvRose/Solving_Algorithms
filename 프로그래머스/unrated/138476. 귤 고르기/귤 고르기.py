27fa84d698e2bba62c56a073b8d1765e1c3ab41c
Jinskim <50815599+MeluvRose@users.noreply.github.com>
2023-08-17 16:43:58 +0900

    [unrated] Title: 귤 고르기, Time: 52.33 ms, Memory: 18 MB -BaekjoonHub

--- 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.py
+++ 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.py
@@ -2,13 +2,17 @@ def solution(k, tangerine):
     answer = 0
     dictTangerine = {}
     
+    # (크기:개수)의 쌍으로 dict 생성
     for t in tangerine:
         if t not in dictTangerine.keys():
             dictTangerine[t] = 1;
             continue;
         dictTangerine[t] += 1;
+    # 개수가 많은 순으로 정렬
     counts = list(dictTangerine.values());
     counts.sort();
+    # 목표 개수를 채울 동안 필요한 개수 채우기
+    # 이 때, 크기는 신경쓰지 않아도 O
     while counts:
         count = counts.pop();
         k -= count;
