9915ac7d2a978c90f854a529802fd69be1a6da10
Jinskim <50815599+MeluvRose@users.noreply.github.com>
2023-08-17 19:20:58 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 50.80 ms, Memory: 19.3 MB -BaekjoonHub

--- 프로그래머스/lv2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.py
+++ 프로그래머스/lv2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.py
@@ -1,19 +1,17 @@
 from collections import deque
-
-def arrSum(arr):
-    result = 0
-    
-    for i in arr: result += i;
-    return result;
     
 def solution(queue1, queue2):
+    sum1 = sum(queue1)
+    sum2 = sum(queue2)
+    # 모든 원소가 같은 위치까지 이동할 만한 횟수로 제한
+    limit = len(queue1) * 3
     answer = 0
-    sum1 = arrSum(queue1)
-    sum2 = arrSum(queue2)
-    limit = len(queue1) * 4;
     
+    # pop(0)보다 더 빠른 popleft()를 사용하기 위한
+    # deque로 전환
     queue1 = deque(queue1);
     queue2 = deque(queue2);
+    # 모든 큐의 값의 합이 홀수면 절대 가능하지 X
     if (sum1 + sum2) % 2 == 1: return -1;
     while sum1 != sum2:
         if answer >= limit: return -1;
