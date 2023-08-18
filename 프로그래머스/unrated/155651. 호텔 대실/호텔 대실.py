1b143c05977c7971f74311f1e73a1fae8ad984cc
Jinskim <50815599+MeluvRose@users.noreply.github.com>
2023-08-18 23:47:20 +0900

    [unrated] Title: 호텔 대실, Time: 1.94 ms, Memory: 10.8 MB -BaekjoonHub

--- 프로그래머스/unrated/155651. 호텔 대실/호텔 대실.py
+++ 프로그래머스/unrated/155651. 호텔 대실/호텔 대실.py
@@ -10,32 +10,55 @@ def solution(book_time):
     answer = 1
     
     bookTime = []
-    # bookQueue = deque()
     
+    # 문자형 -> 숫자형으로 변경
     for start, end in book_time:
         item = []
-        item.append(int(start[:2]) * 60 + int(start[3:]));
-        item.append(int(end[:2]) * 60 + int(end[3:]));
+        item.append(getTime(start));
+        item.append(getTime(end));
         bookTime.append(item);
+    # 시작 시간 기준으로 정렬
     bookTime.sort();
-    # bookTime.sort(key=lambda x:x[1]);
-    # bookQueue = deque(
-    #     sorted(bookQueue, key=lambda x:x[1]));
-    # while(answer < len(bookQueue)):
-    #     if bookQueue[0][1] <= bookQueue[answer][0]:
-    #         bookQueue.pop(0);
-    #     answer += 1;
-    # answer = len(bookQueue);
-    
+    # bookTime.sort(key=lambda x:x[1]); (X)
+    # print(bookTime);
     
     heap = []
     for s, e in bookTime:
+        # print(heap, end = ' ');
         if not heap:
-            heappush(heap,e)
+            heappush(heap,e + 10)
+            # print(heap);
             continue
+        # heap에 의해 heap[0]이 가장 빠른 종료 시각을 저장
+        # heap[0] 보다 새로 입실할 시각이 같거나 느리다면
+        # heap[0] 을 pop, 새로 할당할 공간 생성
         if heap[0] <= s:
             heappop(heap)
+        # heap[0] 보다 새로 입실할 시각이 같거나 느리다면
+        # heap에 할당할 원소의 예상 개수를 추가
         else:
             answer += 1
+        # 새로 들어온 객실의 재입실 가능 시각을 추가 할당
         heappush(heap,e + 10)
+        # print(heap);
     return answer
+
+"""
+1. deque(queue)를 이용한 문제 접근(테케 O, 정답 X)
+    bookQueue = deque()
+    
+    for start, end in book_time:
+        item = []
+        item.append(getTime(start));
+        item.append(getTime(end));
+        bookQueue.append(item);
+    bookQueue = deque(
+        sorted(bookQueue, key=lambda x:x[1]));
+    while(answer < len(bookQueue)):
+        if bookQueue[0][1] <= bookQueue[answer][0]:
+            bookQueue.popleft();
+            bookQueue = deque(
+                sorted(bookQueue, key=lambda x:x[1]));
+            continue;
+        answer += 1;
+"""
