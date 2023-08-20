a82616f3a404820befbaee0a6f57feb05fd8a069
Jinskim <50815599+MeluvRose@users.noreply.github.com>
2023-08-20 22:56:13 +0900

    [unrated] Title: 과제 진행하기, Time: 1.36 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/unrated/176962. 과제 진행하기/과제 진행하기.py
+++ 프로그래머스/unrated/176962. 과제 진행하기/과제 진행하기.py
@@ -5,30 +5,35 @@ def getTime(clock):
 
 def solution(plans):
     answer = []
-    sections = []
-    remains = []
+    sections = [] # 분야(과제)
+    remains = [] # 남은 시간
     clock = 0
     
     plans.sort(key=lambda x:x[1]);
     for p in plans:
         newClock = 0
         
+        # 진행 중인 과제가 없을 경우,
         if not remains: clock = getTime(p[1]); 
         else:
-            # print(remains, p[1]);
+            # 다음 과제가 시작하는 시간
             newClock = getTime(p[1]);
-            # print(newClock - clock);
             while (remains and clock < newClock):
-                # print(newClock, clock, remains[-1]);
+                # 남은 과제가 존재할 때, 가장 최근 과제부터
+                # 새로운 과제가 시작하기 전에, 끝낼 수 있는 과제가
+                # 있는지 체크
                 if (newClock - clock >= remains[-1]):
                     clock += remains.pop();
                     answer.append(sections.pop());
+                # 직전 과제가 끝나지 못할 경우, 남은 시간 재계산
                 else: 
                     remains[-1] -= (newClock - clock);
                     break;
             clock = newClock;
+        # 새로운 과제 및 진행 시간 추가
         sections.append(p[0]);
         remains.append(int(p[2]));
-    # print(sections, remains);
+    # 모든 과제를 확인했을 때, 끝내지 못한 과제가 있다면,
+    # 가장 최근에 중단했던 과제부터 끝내기 시작한다.
     while sections: answer.append(sections.pop());    
     return answer
