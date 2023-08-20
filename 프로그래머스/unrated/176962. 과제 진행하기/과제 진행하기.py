0ca2ac933787c9298dbbec7155d92e99f38e465d
Jinskim <50815599+MeluvRose@users.noreply.github.com>
2023-08-20 23:34:47 +0900

    [unrated] Title: 과제 진행하기, Time: 2.49 ms, Memory: 10.8 MB -BaekjoonHub

--- 프로그래머스/unrated/176962. 과제 진행하기/과제 진행하기.py
+++ 프로그래머스/unrated/176962. 과제 진행하기/과제 진행하기.py
@@ -37,3 +37,19 @@ def solution(plans):
     # 가장 최근에 중단했던 과제부터 끝내기 시작한다.
     while sections: answer.append(sections.pop());    
     return answer
+
+"""
+def solution(plans):
+    plans = sorted(map(lambda x: [x[0], int(x[1][:2]) * 60 + int(x[1][3:]), int(x[2])], plans), key=lambda x: -x[1])
+
+    lst = []
+    while plans:
+        x = plans.pop()
+        for i, v in enumerate(lst):
+            if v[0] > x[1]:
+                lst[i][0] += x[2]
+        lst.append([x[1] + x[2], x[0]])
+    lst.sort()
+
+    return list(map(lambda x: x[1], lst))
+"""
