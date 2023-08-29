298afbb7a494c16242de9b8deb8d17775cdff6c0
Jinskim <50815599+MeluvRose@users.noreply.github.com>
2023-08-29 16:49:11 +0900

    [level 2] Title: 모음 사전, Time: 2.91 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv2/84512. 모음 사전/모음 사전.py
+++ 프로그래머스/lv2/84512. 모음 사전/모음 사전.py
@@ -1,19 +1,34 @@
 def getWords(s):
     result = [];
     
-    # print(result, s);
     if len(s) < 5:
         arr = ['A', 'E', 'I', 'O', 'U']
         for c in arr:
-            result.append(s + c);
+            result.append(s);
             result += getWords(s + c);
     else: result.append(s)
     return result;
+
 def solution(word):
-    # answer = findWord("")
     answer = 0
     words = getWords("")
     
     words = list(set(words));
-    answer = sorted(words).index(word) + 1;
+    answer = sorted(words).index(word);
     return answer
+
+"""
+(product)
+from itertools import product
+
+solution = lambda word: sorted(["".join(c) for i in range(5) for c in product("AEIOU", repeat=i+1)]).index(word) + 1
+"""
+
+"""
+(등비 수열)
+def solution(word):
+    answer = 0
+    for i, n in enumerate(word):
+        answer += (5 ** (5 - i) - 1) / (5 - 1) * "AEIOU".index(n) + 1
+    return answer
+"""
