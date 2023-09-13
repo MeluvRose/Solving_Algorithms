9091b929e51f1b20ef141c89d48405a6812f1387
Jinskim <50815599+MeluvRose@users.noreply.github.com>
2023-09-13 16:37:20 +0900

    [level 2] Title: 타겟 넘버, Time: 1.70 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,10 +1,26 @@
+from itertools import product
+
+def solution(numbers, target):
+    l = [(x, -x) for x in numbers]
+    """
+    *l하는 이유는 'l' 안의 원소들을 
+    하나씩 풀어야 product가 가능
+    (컨테이너 타입의 데이터를 Unpacking)
+    """
+    s = list(map(sum, product(*l)))
+    return s.count(target);
+"""
+(recursion)
 def solution(numbers, target):
     if not numbers:
         if target == 0: return 1;
         return 0;
     return (solution(numbers[1:], target - numbers[0]) 
             + solution(numbers[1:], target + numbers[0]));
+"""    
+    
 """
+(mine)
 answer = 0
 
 def bfs(numbers, depth, value, target):
