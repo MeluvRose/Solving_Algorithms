5738cc001f1dd61257711a87adc9d25fa63ac8b5
Jinskim <50815599+MeluvRose@users.noreply.github.com>
2023-09-14 15:11:34 +0900

    [level 3] Title: 단어 변환, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/lv3/43163. 단어 변환/단어 변환.py
@@ -1,3 +1,30 @@
+from collections import deque
+
+# 한 번에 한 개의 알파벳만 바꿀 수 있습니다.
+transistable = lambda a,b: sum(
+    (1 if x!=y else 0) for x,y in zip(a,b)) == 1;
+
+def solution(begin, target, words):
+    queue = deque()
+    d = dict()
+    
+    queue.append((begin, 0));
+    # 'words'의 요소들은 filter의 x가 되어, 함수
+    # 'transistable' 를 통해 filter가 실행된다.
+    d[begin] = set(filter(
+        lambda x:transistable(x, begin), words));
+    for w in words:
+        d[w] = set(filter(lambda x:transistable(x,w), words));
+    while queue:
+        current, level = queue.popleft()
+        # 탐색 깊이가 'words' 길이보다 클 경우
+        if level > len(words): return 0;
+        for w in d[current]:
+            if w == target: return level + 1;
+            else: queue.append((w, level + 1));
+    return 0;
+"""
+(DFS)
 def solution(begin, target, words):
     lenWords = len(words)
     answer = lenWords + 1
@@ -5,6 +32,10 @@ def solution(begin, target, words):
     visited = [False] * lenWords;
     
     def dfs(word, target, level):
+        # 'visited', 'words'의 경우, 
+        # 전혀 다른 list를 새로 대입(할당)
+        # 하는 것이 아니기 때문에
+        # nonlocal로 선언하지 않아도 O(?)
         nonlocal flag, lenWords, answer
         
         if word == target:
@@ -26,44 +57,4 @@ def solution(begin, target, words):
     dfs(begin, target, 0);
     if flag: return answer;
     return 0;
-
-# def differ(s1, s2):
-#     result = 0
-    
-#     for n in range(len(s1)):
-#         if s1[n] != s2[n]:
-#             result += 1;
-#     return result;
-
-# def transition(begin, target, words):
-#     result = len(words) + 1;
-#     visited = None
-#     stack = []
-    
-#     for w in words:
-#         if differ(begin, w) != 1:
-#             continue;
-#         visited = [w];
-#         stack.append(w);
-#         while stack:
-#             last = stack[-1]
-#             if last == target:
-#                 result = min(len(stack), result); 
-#                 stack.pop();
-#             if visited == words: break;
-#             for new in words:
-#                 if (differ(last, new) == 1
-#                    and new not in visited):
-#                     visited.append(new);
-#                     stack.append(new);
-#                     break;
-#     if result > len(words): return 0;
-#     return result;
-        
-
-# def solution(begin, target, words):
-#     answer = 0
-    
-#     if target not in words: return answer;
-#     answer = transition(begin, target, words);
-#     return answer;
+"""
