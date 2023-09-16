36e56de060af315c742b260e30d33ec6fc9ceb1f
Jinskim <50815599+MeluvRose@users.noreply.github.com>
2023-09-16 17:45:05 +0900

    [unrated] Title: 무인도 여행, Time: 0.37 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/unrated/154540. 무인도 여행/무인도 여행.py
+++ 프로그래머스/unrated/154540. 무인도 여행/무인도 여행.py
@@ -26,9 +26,41 @@ def solution(maps):
                 queue.append((ny, nx))
         return area
     
+    # 방문하지 않은 땅을 찾기
     for y in range(rows):
         for x in range(cols):
             if maps[y][x] != 'X' and visit[y][x] == 0:
                 answer.append(bfs(maps, y, x));
     if answer: return sorted(answer);
     return [-1];
+
+"""
+(DFS)
+
+def dfs(maps, i, j, n, m, visit):
+    visit[i][j] = True
+    area = int(maps[i][j])
+    directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+
+    for dx, dy in directions:
+        x, y = i + dx, j + dy
+        if 0 <= x < n and 0 <= y < m and not visit[x][y] and maps[x][y] != 'X':
+            area += dfs(maps, x, y, n, m, visit)
+
+    return area
+
+def solution(maps):
+    answer = []
+    n, m = len(maps), len(maps[0])
+    visit = [[False] * m for _ in range(n)]
+
+    for i in range(n):
+        for j in range(m):
+            if maps[i][j] != 'X' and not visit[i][j]:
+                answer.append(dfs(maps, i, j, n, m, visit))
+
+    if answer:
+        return sorted(answer)
+    else:
+        return [-1]
+"""
