a08967c185ea071fb842da8e969ce5af37652ecc
Jinskim <50815599+MeluvRose@users.noreply.github.com>
2023-09-20 19:43:29 +0900

    [level 2] Title: 문자열 압축, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/60057. 문자열 압축/문자열 압축.py
+++ 프로그래머스/lv2/60057. 문자열 압축/문자열 압축.py
@@ -1,8 +1,3 @@
-"""
-1. 문자열의 분할은 절반의 길이를 넘을 수 없다.
-2. 
-"""
-
 def solution(s):
     lenS = len(s)
     answer = lenS * 2 if lenS > 1 else 1
@@ -27,3 +22,34 @@ def solution(s):
             idx += sep;
         answer = min(answer, len(compress));
     return answer
+
+"""
+(recursion, java)
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+
+        for(int i=1; i<=(s.length()/2)+1; i++){
+            int result = getSplitedLength(s, i, 1).length();
+            answer = i==1 ? result : (answer>result?result:answer);
+        }
+
+        return answer;
+    }
+
+    public String getSplitedLength(String s, int n, int repeat){
+        if(s.length() < n) return s;
+        String result = "";
+        String preString = s.substring(0, n);
+        String postString = s.substring(n, s.length());
+
+        // 불일치 -> 현재까지 [반복횟수 + 반복문자] 조합
+        if(!postString.startsWith(preString)){
+            if(repeat ==1) return result += preString + getSplitedLength(postString, n, 1);
+            return result += Integer.toString(repeat) + preString + getSplitedLength(postString, n, 1);
+        }
+
+        return result += getSplitedLength(postString, n, repeat+1);
+    }
+}
+"""
