b3f4dffd7c783a7734d5e295cae851533a61e6e2
Jinskim <50815599+MeluvRose@users.noreply.github.com>
2023-09-25 22:59:03 +0900

    [level 2] Title: [1차] 프렌즈4블록, Time: 1.78 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.py
@@ -0,0 +1,26 @@
+def pop_num(b, m, n):
+    pop_set = set()
+    # search
+    for i in range(1, n):
+        for j in range(1, m):
+            if b[i][j] == b[i-1][j-1] == b[i-1][j] == b[i][j-1] != '_':
+                pop_set |= set([(i, j), (i-1, j-1), (i-1, j), (i, j-1)])
+    # set_board
+    for i, j in pop_set:
+        b[i][j] = 0        
+    # 0을 맨 위로 변경하기
+    for i, row in enumerate(b):
+        empty = ['_'] * row.count(0)
+        b[i] = empty + [block for block in row if block != 0]
+    return len(pop_set)
+     
+def solution(m, n, board):
+    count = 0
+    # 행 - 열 바꾸기
+    b = list(map(list,zip(*board)))
+    # print(board);
+    while True:
+        pop = pop_num(b, m, n)
+        if pop == 0: return count
+        count += pop
+    return count;
