3c6abfb357bbf484e0d3ca4c1709a8194842c729
Jinskim <50815599+MeluvRose@users.noreply.github.com>
2023-10-10 15:20:45 +0900

    [level 2] Title: 가장 큰 정사각형 찾기, Time: 757.58 ms, Memory: 38.6 MB -BaekjoonHub

--- 프로그래머스/lv2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.py
+++ 프로그래머스/lv2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.py
@@ -1,34 +1,7 @@
 def solution(board):
-    line = 0
-    rows = len(board)
-    cols = len(board[0])
-    points = [[0] * (cols + 1) 
-              for _ in range(rows + 1)]
-    
-    """
-    # print(board);
-    for r in range(rows - 1):
-        for c in range(cols - 1):
-            if (board[r][c] != 0
-               and board[r + 1][c] != 0
-               and board[r][c + 1] != 0
-               and board[r + 1][c + 1] != 0):
-                board[r + 1][c + 1] = board[r][c] + 1;
-    for row in board:
-    #     print(row);
-        maxLen = max(row);
-        answer = max(answer, maxLen * maxLen);
-    """
-    for r in range(rows):
-        for c in range(cols):
-            points[r + 1][c + 1] = board[r][c];
-    for r in range(rows + 1):
-        for c in range(cols + 1):
-            if points[r][c] != 0:
-                points[r][c] = min(
-                    min(points[r - 1][c], points[r][c - 1]), points[r - 1][c - 1]
-                ) + 1
-                line = max(line, points[r][c]);
-    # for row in points:
-    #     print(row);
-    return line * line;
+    answer = 1234
+
+    # [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    print('Hello Python')
+
+    return answer
