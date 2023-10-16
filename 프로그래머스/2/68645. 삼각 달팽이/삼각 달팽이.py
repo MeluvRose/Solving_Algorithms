ff20bf09b8e25f31fd79d49121b446a95abcd5c9
Jinskim <50815599+MeluvRose@users.noreply.github.com>
2023-10-16 11:12:48 +0900

    [level 2] Title: 삼각 달팽이, Time: 106.34 ms, Memory: 43.2 MB -BaekjoonHub

--- 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.py
+++ 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.py
@@ -1,16 +1,8 @@
-# def sumToN(n):
-#     if n > 1:
-#         return n + sumToN(n - 1);
-#     return n;
-
 def solution(n):
     answer = []
-    # maxVal = sumToN(n)
     snail = [[0] * (i + 1) for i in range(n)]
     row, col = -1, 0
     val = 1
-    # while val < (maxVal + 1):
-    # for val in range(1, maxVal + 1):
     for i in range(n):
         """
         채우기 연산의 방향은 3가지 순서로 반복된다.
@@ -33,3 +25,48 @@ def solution(n):
     for s in snail:
         answer.extend(s);
     return answer
+
+"""
+(class)
+import sys
+sys.setrecursionlimit(1000000)
+
+class Triangle():
+    def __init__(self,n):
+        self.board = [[0 for _ in range(n)] for _ in range(n)]
+        self.cnt = 1
+        self.answer = []
+        self.ldown(0,0,n)
+        for i in range(n):
+            for j in range(i+1):
+                self.answer.append(self.board[i][j])
+
+    def ldown(self,x,y,n):
+        if n < 1:
+            return 0
+        for i in range(x,x+n):
+            self.board[i][y] = self.cnt
+            self.cnt += 1
+        self.right(i,y+1,n-1)
+
+    def right(self,x,y,n):
+        if n < 1:
+            return 0
+        for j in range(y,y+n):
+            self.board[x][j] = self.cnt
+            self.cnt += 1
+        self.lup(x-1,j-1,n-1)
+
+    def lup(self,x,y,n):
+        if n < 1:
+            return 0
+        for i in range(x,x-n,-1):
+            self.board[i][y] = self.cnt
+            y -= 1
+            self.cnt += 1
+        self.ldown(i+1,y+1,n-1)
+
+def solution(n):
+    triangle = Triangle(n)
+    return triangle.answer
+"""
