0ed42303ed4e71a9a87b3274f7e511599728eab9
Jinskim <50815599+MeluvRose@users.noreply.github.com>
2023-10-24 17:39:56 +0900

    [level 3] Title: 기지국 설치, Time: 4.11 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/12979. 기지국 설치/README.md
+++ 프로그래머스/3/12979. 기지국 설치/README.md
@@ -1,10 +1,10 @@
 # [level 3] 기지국 설치 - 12979 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12979) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12979?language=python3) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 4.00 ms
+메모리: 10.3 MB, 시간: 4.11 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 10월 2일 17:33:46
+2023년 10월 2일 17:39:43
 
 ### 문제 설명
 
--- 프로그래머스/3/12979. 기지국 설치/기지국 설치.py
+++ 프로그래머스/3/12979. 기지국 설치/기지국 설치.py
@@ -1,22 +1,19 @@
-"""
-1. 새로운 기지국은 전파가 닿지 않는 아파트에 필요
-2. 기존에 설치된 기지국을 기준으로 구역을 나눠준다.
-    (만약, stations의 길이가 m이라면, m + 1의 구역이 나올 것)
-3. 나눠준 구역은 전부 빈 공간이기에 그 공간에 필요한 최소 기지국
-수를 구함
-"""
-
 def solution(n, stations, w):
     answer = 0
     idx = 0
     
     i = 1
     while i <= n:
+        # 현재 위치에서 전파가 닿을 때,
         if (idx < len(stations)
             and i >= stations[idx] - w):
+            # 위치를 전파가 안 닿는 곳까지 이동
             i = stations[idx] + w + 1;
             idx += 1;
+        # 현재 위치에서 전파가 안 닿을 때,
         else:
-            answer += 1;
+            # 현재 위치를 포함해서, 전파 범위만큼 이동
+            # (새로운 기지국이 이 범위만큼 전파가 가능하다고 가정)
             i += w * 2 + 1;
+            answer += 1;
     return answer;
