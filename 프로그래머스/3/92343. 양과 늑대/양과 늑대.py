c34dffb5a720607f06641ff0296e44fb5bdaffb7
Jinskim <50815599+MeluvRose@users.noreply.github.com>
2023-11-29 11:36:02 +0900

    [level 3] Title: 양과 늑대, Time: 0.96 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.py
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.py
@@ -1,6 +1,8 @@
-# 이 문제는 dfs를 통한 완전 탐색이긴 하지만,
-# 각 탐색 분기의 결과는 서로 독립적이고
-# 서로 비교되는 대상에 해당한다.
+"""
+이 문제는 dfs(bfs)를 통한 완전 탐색이긴 하지만,
+각 탐색 분기의 결과는 서로 독립적이고
+서로 비교되는 대상에 해당한다.
+"""
 
 def solution(info, edges):
     answer = 0
@@ -34,3 +36,50 @@ def solution(info, edges):
     
     solve(visited, [0, 0, 0]);
     return answer;
+
+"""
+(BFS)
+from collections import deque
+
+def solution(info, edges):
+    hash = {}
+    for i in range(len(info)):
+        hash[i] = []
+
+    for s, e in edges:
+        hash[s].append(e)
+        hash[e].append(s)
+
+    # bfs
+    queue = deque()
+    visited = [[0] * (1 << 17) for _ in range(len(info))]
+
+    # cur, sheep, wolf, visited
+    queue.append([0, 1, 0, 1])
+    visited[0][1] = True;
+
+    answer = 0
+
+    while len(queue) > 0:
+        cur, s, w, mask = queue.popleft()
+        answer = max(answer, s)
+        for node in hash[cur]:
+            ns = nw = 0
+            if info[node] == 0 and mask & (1 << node) == 0:
+                ns = 1
+            if info[node] == 1 and mask & (1 << node) == 0:
+                nw = 1
+
+            if s <= w + nw:
+                continue
+
+            nmask = mask | (1 << node)
+
+            if visited[node][nmask]:
+                continue
+
+            visited[node][nmask] = True
+            queue.append([node, s + ns, w + nw, nmask])
+
+    return answer
+"""
